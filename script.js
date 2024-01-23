a114fc21e396b4a53ad9663cf76d687f10242200
mmsalmanfaris <mmsalmanfaaris01@gmail.com>
2024-01-23 06:15:54 +0530

    Refactor HTML and JavaScript code for AI Image Generator

--- script.js
+++ script.js
@@ -1,46 +1,78 @@
-const generateform = document.querySelector(".genarate-form");
-const imagegallery = document.querySelector(".image-gallery");
+const generateForm = document.querySelector(".generate-form");
+const generateBtn = generateForm.querySelector(".generate-btn");
+const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "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"; 
+let isImageGenerating = false;
 
-const genarateaiimages = async (userprompt, userimagequantity) => {
-    try{
-        const response = await fetch("https://api.openai.com/v1/images/generations", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${OPENAI_API_KEY}`
-            },
-            body: JSON.stringify({
-                prompt: userprompt,
-                n: parseInt(userimagequantity),
-                size: '512x512',
-                response_format: "b64_json"
-            })
-        })
-        const { data } = await response.json();
-        console.log(Date)
-    }catch (error){
-        console.log(error);
+const updateImageCard = (imgDataArray) => {
+  imgDataArray.forEach((imgObject, index) => {
+    const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+    const imgElement = imgCard.querySelector("img");
+    const downloadBtn = imgCard.querySelector(".download-btn");
+    
+    const aiGeneratedImage = `data:image/jpeg;base64,${imgObject.b64_json}`;
+    imgElement.src = aiGeneratedImage;
+    
+    imgElement.onload = () => {
+      imgCard.classList.remove("loading");
+      downloadBtn.setAttribute("href", aiGeneratedImage);
+      downloadBtn.setAttribute("download", `${new Date().getTime()}.jpg`);
     }
+  });
 }
 
-const handleFormSubmission = (e) => {
-    e.preventDefault();
+const generateAiImages = async (userPrompt, userImgQuantity) => {
+  try {
+    const response = await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        prompt: userPrompt,
+        n: userImgQuantity,
+        size: "512x512",
+        response_format: "b64_json"
+      }),
+    });
 
-    const userprompt = e.srcElement[0].value;
-    const userimagequantity = e.srcElement[1].value;
+    if(!response.ok) throw new Error("Failed to generate AI images. Make sure your API key is valid.");
 
-    const imgcardmarkup = Array.from({length: userimagequantity}, ()=>
-        `<div class="image-card loading">
-        <img src="images/loader.svg" alt="image">
-        <a href="#" class="download_btn">
-            <img src="images/download.svg" alt="download icon">
+    const { data } = await response.json(); 
+    updateImageCard([...data]);
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    generateBtn.removeAttribute("disabled");
+    generateBtn.innerText = "Generate";
+    isImageGenerating = false;
+  }
+}
+
+const handleImageGeneration = (e) => {
+  e.preventDefault();
+  if(isImageGenerating) return;
+
+  const userPrompt = e.srcElement[0].value;
+  const userImgQuantity = parseInt(e.srcElement[1].value);
+  
+  generateBtn.setAttribute("disabled", true);
+  generateBtn.innerText = "Generating";
+  isImageGenerating = true;
+  
+  const imgCardMarkup = Array.from({ length: userImgQuantity }, () => 
+      `<div class="img-card loading">
+        <img src="images/loader.svg" alt="AI generated image">
+        <a class="download-btn" href="#">
+          <img src="images/download.svg" alt="download icon">
         </a>
-    </div>`
-    ).join("");
+      </div>`
+  ).join("");
 
-    imagegallery.innerHTML = imgcardmarkup;
+  imageGallery.innerHTML = imgCardMarkup;
+  generateAiImages(userPrompt, userImgQuantity);
 }
 
-generateform.addEventListener("submit", handleFormSubmission);
+generateForm.addEventListener("submit", handleImageGeneration);
